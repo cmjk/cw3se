4d00be968d2aee59c7bfce7389c1de861684b5a2
Dzmitry <dzmitry_skarabahatau@epam.com>
2018-06-25 18:05:15 +0300

    Rewrote parsing using classes; getting ready to parse for auction lots

--- listener.py
+++ listener.py
@@ -11,26 +11,59 @@ api_hash = cfg['api_hash']
 phone = cfg['phone']
 
 
-class Transaction:
+class ParsedMessage:
+    def __init__(self, timestamp):
+        self.timestamp = timestamp
+
+    def to_json(self):
+        self.timestamp = self.timestamp.isoformat()
+        return self.__dict__
+
+    def to_string(self):
+        return json.dumps(self.to_json())
+
+
+class Transaction(ParsedMessage):
     def __init__(self, timestamp, resource, quantity, price):
-        self.timestamp = timestamp.isoformat()
+        super().__init__(timestamp)
         self.resource = resource
         self.quantity = int(quantity)
         self.price = int(price)
         self.total = self.price * self.quantity
 
 
-def message_parser(message):
-    date = message.date
-    msg = message.message
-    resource = ''
+class Lot(ParsedMessage):
+    def __init__(self, timestamp, item, price, status):
+        super().__init__(timestamp)
+        self.item = item
+        self.price = price
+        self.status = status
 
-    for line in msg.splitlines():
-        if line[0] in string.ascii_uppercase:
-            resource = line[0:-1]
-        else:
-            q, p = line.split(',')[-1][1:-1].split(' x ')
-            return Transaction(date, resource, q, p).__dict__
+    def is_active(self):
+        return self.status == '#active'
+
+
+class Message:
+    def __init__(self, raw_msg):
+        self.date = raw_msg.date
+        self.msg = raw_msg.message
+
+    def parse_transaction(self):
+        resource = ''
+
+        for line in self.msg.splitlines():
+            if line[0] in string.ascii_uppercase:
+                resource = line[0:-1]
+            else:
+                q, p = line.split(',')[-1][1:-1].split(' x ')
+                return Transaction(self.date, resource, q, p)
+
+    def parse_lot(self):
+        lines = self.msg.splitlines()
+        item = lines[1].split(' : ')[1]
+        price = lines[2].split(' ')[2]
+        status = lines[5].split(': ')[1]
+        return Lot(self.date, item, price, status)
 
 
 client = TelegramClient('session', api_id, api_hash, update_workers=1, spawn_read_thread=False)
@@ -39,7 +72,8 @@ client.start(phone=phone)
 
 @client.on(events.NewMessage(chats=client.get_entity('https://t.me/cwExchange')))
 def my_event_handler(event):
-    print(message_parser(event.message))
+    with open('data2.txt', 'a') as f:
+        f.write(json.dumps(Message(event.message).parse_transaction().to_json()) + '\n')
 
 
 client.idle()
